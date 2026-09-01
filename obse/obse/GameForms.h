949056597ccb64c6fdc23a59564128e61b072778
Brandon <brandonahimes@gmail.com>
2026-09-01 15:13:27 -0700

    Added AnimSequence structs from TESReloaded

--- obse/obse/GameForms.h
+++ obse/obse/GameForms.h
@@ -4511,3 +4511,27 @@ public:
 };
 STATIC_ASSERT(sizeof(ActorSkinInfo) == 0x154);
 
+class AnimSequenceBase {
+public:
+	virtual void					Destructor(UInt8 Arg);
+	virtual void					AddAnimGroupSequence(BSAnimGroupSequence* AnimGroupSequence);
+	virtual void					Unk_02();
+	virtual UInt8					IsSingle();
+	virtual BSAnimGroupSequence* GetAnimGroupSequence(int Index); // Index is not used if Single (returns the anim); Index = -1 returns a random anim in the NiTList<BSAnimGroupSequence>* for Multiple
+	virtual void					Unk_05();
+};
+STATIC_ASSERT(sizeof(AnimSequenceBase) == 0x004);
+
+class AnimSequenceSingle : public AnimSequenceBase {
+public:
+	BSAnimGroupSequence* Anim;		// 04
+};
+STATIC_ASSERT(sizeof(AnimSequenceSingle) == 0x008);
+
+class AnimSequenceMultiple : public AnimSequenceBase {
+public:
+	
+	NiTPointerList<BSAnimGroupSequence>* Anims;	// 04
+};
+STATIC_ASSERT(sizeof(AnimSequenceMultiple) == 0x008);
+
